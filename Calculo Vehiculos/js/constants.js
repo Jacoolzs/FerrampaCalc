8e107936eacbb97d7e6cc70a92bc16148165fd2a
Jacoolzs <147072784+Jacoolzs@users.noreply.github.com>
2026-04-04 21:13:42 -0400

    Delete Calculo Vehiculos directory

--- Calculo Vehiculos/js/constants.js
+++
@@ -1,2 +0,0 @@
-export const CURRENT_YEAR = 2026;
-export const IMPORT_LIMIT_YEAR = CURRENT_YEAR - 5;
--- Calculo Vehiculos/js/main.js
+++
@@ -1,9 +0,0 @@
-import { initApp } from './ui.js';
-
-// En módulos, el código ya se ejecuta después de que el DOM está listo por defecto,
-// pero usar el evento es una buena práctica.
-if (document.readyState === 'loading') {
-    document.addEventListener('DOMContentLoaded', initApp);
-} else {
-    initApp();
-}
